005537f16f9f746ab470cd01b561fd100a9106ed
K12-K <kalp12122000@gmail.com>
2026-04-24 18:39:01 +0530

    updated socket url from env var

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://your-backend.com", {
+export const socket = io(process.env.SERVICE_URL, {
   transports: ["websocket"],
 });
